83a70dc24772646fbd54749c239a2de949a8eff3
System <none>
2017-09-03 05:36:42 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -15,7 +15,18 @@ app.use(express.static('public'));
 //mongoose.Promise = global.Promise;
 mongo.connect("mongodb://gunnja:gunnja@ds123124.mlab.com:23124/fccmongo",(err, database) => {
   if (err) return console.log(err)
+
   console.log(database);
+});
+
+const db = mongo.connection;
+
+db.on("error", (err) => {
+	console.error(`connection error: ${err}`);
+});
+
+db.once("open", () =>{
+	console.log("db connection successful");
 });
 
 //const db = mongo.connection;
