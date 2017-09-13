0974eafdeb7541719e7dc8c021e678505f1541a1
System <none>
2017-09-13 03:21:15 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 
 // init project
 const key = process.env.KEY;
+const cx = process.env.CX;
 const express = require('express');
 const app = express();
 const mongo = require('mongodb').MongoClient
@@ -11,75 +12,6 @@ const APIurl = "https://www.googleapis.com/customsearch/v1?"
 const dbCollection = "fccsearch";
 let database;
 let collect;
-
-// functions
-function dbInsert(collection,obj,res) {
-  collection.insert(obj, function(err, data) {
-    if (err) throw err
-    database.close;
-    res.send({"Shortened URL": "https://urlshortener-10.glitch.me/" + obj.quickID});
-  })
-}
-
-function finder(collection, queryObj) {
-    return collection.find(queryObj);
-}
-
-function dbFind(collection,id,res) {
-  let promFind = new Promise(function(resolve, reject) {
-    collection.findOne({ 'quickID' : { $eq: parseInt(id) }}, function(err, item) {
-      if (err) {
-        throw err;
-      } else if (item === null) {
-         res.send({"error": "ID doesn't exist"});
-      } else {
-      resolve(item.path);
-      }
-    });
-  })
-  promFind.then(function(path) {
-        console.log("path",path);
-        if (path) {
-          res.redirect(path);
-          database.close;
-        } else {
-          database.close;
-        }
-    });
-}
-
-function promQuery(collection, queryObj) {
-  return new Promise(function(resolve, reject) {
-			resolve(finder(collection, queryObj).count());
-		});
-}
-
-function exists(collection, req, res, queryObj) {
-  return new Promise(function(resolve, reject) {
-    promQuery(collection, queryObj).then(function(count) {
-        console.log("dbExists:",count);
-        if (count > 0) {
-          reject(Error("already exists"))
-          res.send({"error": "already exists"});
-          database.close;
-        } else {
-          resolve(assignID(collection, req, res, {}));
-        }
-    })
-  })
-}
-
-function assignID(collection, req, res, queryObj) {
-  promQuery(collection, queryObj).then(function(count) {
-      console.log("path", req.path.substring(1))
-      let obj = {'quickID' : count, 'path' : req.path.substring(1)};
-      console.log("createObj",obj);
-      return obj;
-  }).then(function(obj) {
-      console.log("new entry");
-      dbInsert(collection, obj, res);
-    });
-}
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
