4ee88008d89d11dc259004435364457d728dd225
System <none>
2017-09-13 04:38:15 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -17,41 +17,17 @@ let collect;
 var google = require('googleapis');
 var customsearch = google.customsearch('v1');
 
-function imageSearch() customsearch.cse.list({ cx: cx, q: "dogs", auth: key, searchType: "image", num: 10 }, function (err, resp) {
-  if (err) {
-    return console.log('An error occured', err);
-  }
-  // Got the response from custom search
-  console.log('Result: ' + resp.searchInformation.formattedTotalResults);
-  console.log(resp);
-});
-}
-
-let options = {
-  hostname: APIurl,
-  port: 8080,
-  path: '/customsearch/v1?q=%22stars%22&cx=015452621321954620393%3Aqo5wx60lbq0&key=',
-  method: 'GET'
-}
-
-const req = http.request(options, (res) => {
-  console.log(`STATUS: ${res.statusCode}`);
-  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-  res.setEncoding('utf8');
-  res.on('data', (chunk) => {
-    console.log(`BODY: ${chunk}`);
+// Function to search google images
+function imageSearch(q,num) {
+  customsearch.cse.list({ cx: cx, q: q, auth: key, searchType: "image", num: num }, function (err, resp) {
+    if (err) {
+      return console.log('An error occured', err);
+    }
+    // Got the response from custom search
+    console.log('Result: ' + resp.searchInformation.formattedTotalResults);
+    console.log(resp);
   });
-  res.on('end', () => {
-    console.log('No more data in response.');
-  });
-});
-
-req.on('error', (e) => {
-  console.error(`problem with request: ${e.message}`);
-});
-
-//req.write(postData);
-req.end();
+}
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -70,8 +46,9 @@ mongo.connect("mongodb://gunnja:gunnja@ds131854.mlab.com:31854/fccdb",(err, db) 
 });
 
 // Get new urls
-app.get(/^\/(http\:\/\/|https\:\/\/).+/, function (req, res) {
-
+app.get(/^\/search\/.+/, function (req, res) {
+  let searchQ = req.path.split("/");
+  searchQ[2];
 })
 
 app.get("/urls", function(req, res) {
