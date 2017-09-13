cc4d12425a5d3743963dae803236a3e80df0e6db
System <none>
2017-09-13 03:32:15 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -6,12 +6,34 @@ const key = process.env.KEY;
 const cx = process.env.CX;
 const express = require('express');
 const app = express();
+const http = require('http');
 const mongo = require('mongodb').MongoClient
 const url = require('url');
 const APIurl = "https://www.googleapis.com/customsearch/v1?"
 const dbCollection = "fccsearch";
 let database;
 let collect;
+
+let options = {
+  hostname: 'www.googleapis.com',
+  port: 80,
+  path: '/customsearch/v1?cx=015452621321954620393:qo5wx60lbq0&q=dog&key=AIzaSyCRzwpBjrvOl-3mWeJRQLUcNwoSoioo3bw',
+  method: 'GET'
+}
+
+const req = http.request(options, (res) => {
+  console.log(`STATUS: ${res.statusCode}`);
+  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+  res.setEncoding('utf8');
+  res.on('data', (chunk) => {
+    console.log(`BODY: ${chunk}`);
+  });
+  res.on('end', () => {
+    console.log('No more data in response.');
+  });
+});
+
+req;
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -31,26 +53,16 @@ mongo.connect("mongodb://gunnja:gunnja@ds131854.mlab.com:31854/fccdb",(err, db) 
 
 // Get new urls
 app.get(/^\/(http\:\/\/|https\:\/\/).+/, function (req, res) {
-  exists(collect, req, res, {
-    path : { $eq: req.path.substring(1) }
-  })
+
 })
 
 app.get("/urls", function(req, res) {
-  let promRetr = new Promise(function (resolve, reject) {
-    finder(collect,{}).toArray(function (err,data) {
-      let urls = data;
-      resolve(urls);
-    });
-  }).then(function(urls) {
-    res.send(urls);
-  })
+
 });
   
 // Redirect existing shortened urls
 app.get(/\d+/, function (req, res) {
-  let id = req.path.substring(1);
-  dbFind(collect,id,res)
+
 })
 
 // listen for requests :)
