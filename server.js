2d65b74a83eac1de4f199eb1ebe1f09a6447d9f2
System <none>
2017-09-19 05:39:14 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -13,22 +13,16 @@ let offsetDefault = 10;
 let database;
 let collect;
 
-var google = require('googleapis');
-var customsearch = google.customsearch('v1');
-
-// Google Images API call
-function imageSearch(q,num) {
-  return new Promise(function (resolve, reject) {
-    customsearch.cse.list({ cx: cx, q: q, auth: key, searchType: "image", num: num }, function (err, resp) {
-      if (err) {
-        return console.log('An error occured', err);
-      }
-      // Got the response from custom search
-      console.log('Result: ' + resp.searchInformation.formattedTotalResults);
-      resolve(resp);
-    });
-  });
-}
+var express = require('express')
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+ 
+var app = express()
+ 
+app.post('/profile', upload.single('avatar'), function (req, res, next) {
+  // req.file is the `avatar` file 
+  // req.body will hold the text fields, if there were any 
+})
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -38,55 +32,7 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-//DB functions
-mongo.connect('mongodb://gunnja:gunnja@ds131854.mlab.com:31854/fccdb',(err, db) => {
-  if (err) throw err
-  else console.log("db connection successful")
-  collect = db.collection(dbCollection);
-  database = db;
-// db.close();
-});
 
-function dbInsert(collection,obj) {
-  collection.insert(obj, function(err, data) {
-    if (err) throw err
-    database.close;
-    console.log("insert complete");
-  })
-}
-
-function finder(collection) {
-  return collection.find({});
-}
-
-//Routing
-app.get("/search/:query", function (req, res) {
-  const searchQ = req.params.query;
-  dbInsert(collect,{ "query": searchQ })
-  if (req.query.offset) {
-    console.log("offset", req.query.offset);
-    console.log("searchQ", searchQ);
-    imageSearch(searchQ,req.query.offset).then(function(data) {
-      res.send(data.items);
-    });
-  }else {
-    console.log("no offset", searchQ);
-    imageSearch(searchQ,offsetDefault).then(function(data) {
-      res.send(data.items);
-    });
-  }
-});
-
-app.get("/recent", function (req, res) {
-  let promRetr = new Promise(function (resolve, reject) {
-    finder(collect).toArray(function (err,data) {
-      resolve(data);
-    })
-  })
-  promRetr.then(function(data) {
-    res.send(data);
-  })
-});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
