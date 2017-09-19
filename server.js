a9b954135da3d83beba797c90b25bf627ca7d02e
GunnJA <30766340+GunnJA@users.noreply.github.com>
2017-09-19 17:12:31 +1000

    Update server.js
    removed some code

--- server.js
+++ server.js
@@ -6,12 +6,6 @@ const express = require('express');
 const app = express();
 var multer  = require('multer')
 var upload = multer({ dest: 'uploads/' })
- 
-app.post('/profile', upload.single('avatar'), function (req, res, next) {
-  // req.file is the `avatar` file 
-  
-  // req.body will hold the text fields, if there were any 
-})
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
