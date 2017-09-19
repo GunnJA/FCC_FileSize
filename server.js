e179eb6d52289a0ef00161889d9b81dba9f4e0bf
System <none>
2017-09-19 05:50:14 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -2,25 +2,14 @@
 // where your node app starts
 
 // init project
-const key = process.env.KEY;
-const cx = process.env.CX;
-const mongoURL = process.env.MURL;
 const express = require('express');
 const app = express();
-const mongo = require('mongodb').MongoClient
-const dbCollection = "fccsearch";
-let offsetDefault = 10;
-let database;
-let collect;
-
-var express = require('express')
 var multer  = require('multer')
 var upload = multer({ dest: 'uploads/' })
  
-var app = express()
- 
 app.post('/profile', upload.single('avatar'), function (req, res, next) {
   // req.file is the `avatar` file 
+  
   // req.body will hold the text fields, if there were any 
 })
 
@@ -28,9 +17,14 @@ app.post('/profile', upload.single('avatar'), function (req, res, next) {
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
 });
+
+app.get("/open_file", function (req, res) {
+  console.log(req.body);
+});
+
 
 
 
